d8d44ead7dd4ab73ce18df9df14adaa343c13c31
Siddhant Chutke <49346790+sid77x@users.noreply.github.com>
2023-10-10 00:09:57 +0530

    Update q1_bubblesort.cpp
    solved the qn

--- week1/q1_bubblesort.cpp
+++ week1/q1_bubblesort.cpp
@@ -1,8 +1,33 @@
-#include <iostream.h>
-#include <stdio.h>
+#include <iostream>
 
-void main()
+
+using namespace std;
+int main()
 {
-  char a[50];
-  //cin karna hai , syntax revise kar
+    
+    cout<<"Enter the number of letters";
+    int n;
+    cin>>n;
+    char a[n];
+    for(int i = 0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<n-1; j++)
+        {
+        if(a[j]>a[j+1])
+        {
+            char temp = a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
+        }
+        }
+    }
+
+    for(int i = 0; i<n; i++)
+    {
+        cout<<a[i];
+    }
 }
